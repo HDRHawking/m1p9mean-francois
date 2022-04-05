326e0680e4391ada54480a70fd640d33ff476e87
Francois Rakoto <50391876+HDRHawking@users.noreply.github.com>
2022-04-05 16:09:06 +0300

    debug crud

--- m1p9mean-back/function.js
+++ m1p9mean-back/function.js
@@ -20,24 +20,8 @@ module.exports = {
         return res.rows[0].nb;
     },
 
-    insert : async function($listeChamp,$listeValues,$table,$column_unique = 'id'){
-        var sqlChamp = "insert into "+$table+"( ";
-        var sqlValue = "values ( ";
-        for($i = 0;$i<$listeChamp.length;$i++){
-            if($listeChamp[$i] == $column_unique){
-                sqlChamp += ""+$listeChamp[$i]+" ,";
-                sqlValue += ""+$listeValues[$i]+" ,";               
-            } else if($i==$listeChamp.length-1){
-                sqlChamp += ""+$listeChamp[$i]+" )";
-                sqlValue += "'"+$listeValues[$i]+"' )";
-            } else {
-                sqlChamp += ""+$listeChamp[$i]+" ,";
-                sqlValue += "'"+$listeValues[$i]+"' ,";
-            } 
-        }
-        var sql = sqlChamp+sqlValue;
-        let result = await con.query(sql);
-        return result.rowCount;
+    insert : async function($table,$object){
+        return await client.db("mean_v1").collection($table).insertOne($object);
     },
 
     delete : async function($condition,$table){
@@ -51,18 +35,7 @@ module.exports = {
         return result.rowCount;
     },
 
-    update : async function($listeChamp,$listeValues,$table,$condition){
-        var sql =  "update "+$table +" set ";
-        for($i = 0;$i<$listeChamp.length;$i++){
-            if($i==$listeChamp.length-1){
-                sql += ""+$listeChamp[$i]+" = '"+$listeValues[$i]+"' where ";
-            } else {
-                sql += ""+$listeChamp[$i]+" = '"+$listeValues[$i]+"',";
-            } 
-        }
-        sql += $condition;
-        console.log(sql);
-        let result = await con.query(sql);
-        return result.rowCount;
+    update : async function($objectupdate,$table,$condition){
+        return await client.db("mean_v1").collection($table).updateOne($condition,$objectupdate);
     }
 }
